e4f1162cc32ba669df696bb5b337ef5d241fdf23
Kerry Russo <kerry.kim.russo@gmail.com>
2018-01-10 20:57:36 -0500

    Fetch nearby places from API using image long and lat

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
+import dotenv from 'dotenv';
 import { ExifImage } from 'exif';
- 
-try {
-    new ExifImage({ image : 'assets/images/img_1.JPG' }, function (error, exifData) {
-        if (error)
-            console.log('Error: '+error.message);
-        else
-            console.log(exifData); // Do something with your data! 
-    });
-} catch (error) {
-    console.log('Error: ' + error.message);
-}
+import fetch from 'fetch';
+
+dotenv.config();
+
+const convertExifGpsToDegrees = (ref, [decimal,mins,secs]) => {
+  const hemisphereMultiplier = /[EN]/.test(ref) ? 1 : -1;
+  return hemisphereMultiplier * (decimal + mins/60 + (secs * 2)/3600);
+};
+
+const exif = new ExifImage({ image : 'assets/images/img_1.JPG' }, function (error, exifData) {
+  if (error) return console.log('Error: ' + error.message);
+  
+  const {
+    GPSLatitudeRef,
+    GPSLatitude,
+    GPSLongitudeRef,
+    GPSLongitude,
+  } = exifData.gps;
+  const lat = convertExifGpsToDegrees(GPSLatitudeRef, GPSLatitude);
+  const lon = convertExifGpsToDegrees(GPSLongitudeRef, GPSLongitude);
+
+  const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${lon}&radius=${1000}&key=${process.env.PLACES_API_KEY}`;
+  
+  fetch.fetchUrl(url, (error, meta, body) => {
+    const data = JSON.parse(body.toString());
+    console.log(data.results.map(x => x.name));
+  });
+});
